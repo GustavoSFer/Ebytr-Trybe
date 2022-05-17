7cd37db1493fd382689bb51ce729cf9f4964954f
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-17 17:06:57 -0300

    test: model find

---
+++ back-end/src/tests/model/model.test.js
@@ -0,0 +1,35 @@
+const listModel = require('../../Model/listModel');
+const { returnDb, taskBody } = require('../MockDb/task');
+const model = require('../../ConnectionMongo');
+
+const { expect } = require('chai');
+const Sinon = require('sinon');
+
+describe('ListModel', () => {
+  describe('#find', () => {
+   describe('Quando existe dados no banco', () => {
+    before(() => {
+      Sinon.stub(model, 'find').resolves(returnDb);
+    });
+    after(() => {
+      Sinon.restore();
+    });
+    it('Deve retornar uma lista quando tem dados no banco', async () => {
+      const taskList = await listModel.getAll();
+      expect(taskList).to.deep.eq(returnDb);
+    });
+   });
+   describe('Quando não existe dados no banco', () => {
+    before(() => {
+      Sinon.stub(model, 'find').resolves([]);
+    });
+    after(() => {
+      Sinon.restore();
+    });
+    it('Deve retornar um array vazio', async () => {
+      const taskList = await listModel.getAll();
+      expect(taskList).to.be.empty;
+    });
+  })
+  });
+});
