83dc3dce0da51743ee7e4e318bec91b76cd3166e
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-18 17:39:42 -0300

    Feat: Criando validação do put - status para alterar

--- back-end/src/Routes/index.js
+++ back-end/src/Routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const listController = require('../Controller/listController');
-const { validationTask } = require('../middleware/validationTask');
+const { validationPost, validationPut } = require('../middleware/validationTask');
 
 const route = Router();
 
 route.get('/', listController.getAll);
-route.post('/', validationTask, listController.postList);
-route.put('/', listController.updateTask);
+route.post('/', validationPost, listController.postList);
+route.put('/', validationPut, listController.updateTask);
 route.delete('/', listController.remove);
 
 module.exports = route;
--- back-end/src/middleware/validationTask.js
+++ back-end/src/middleware/validationTask.js
@@ -1,10 +1,17 @@
-const validationTask = (req, res, next) => {
+const validationPost = (req, res, next) => {
   const { task, status } = req.body;
   if (!task) return next({ code: 400, message: 'Favor informar a mensagem da tarefa' });
   if (!status) return next({ code: 400, message: 'Favor informar o status da tarefa' });
   return next();
 };
 
+const validationPut = (req, res, next) => {
+  const { status } = req.body;
+  if (!status) return next({ code: 400, message: 'Favor informar o status da tarefa' });
+  return next();
+};
+
 module.exports = {
-  validationTask,
+  validationPost,
+  validationPut,
 };
