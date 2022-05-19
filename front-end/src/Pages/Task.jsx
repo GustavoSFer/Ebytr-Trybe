689d6d4ca03a988f1bdda116b463a5454da0b9a5
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-05-19 11:45:43 -0300

    Feat: Criando um botão

--- front-end/src/Pages/Task.jsx
+++ front-end/src/Pages/Task.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import ButtonGeneric from '../Components/ButtonGeneric';
 
 function Task() {
+  const hadleClick = () => {
+    console.log('foi clicado');
+  }
+
   return (
     <main>
-      
+      <section>
+        <ButtonGeneric click={ hadleClick }>Cadastrar tarefa</ButtonGeneric>
+      </section>
+      <section>
+       
+      </section>
     </main>
   );
 };
